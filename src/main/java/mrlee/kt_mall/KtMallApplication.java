d38d43687d0eef83126ae337c9d5c09fcbdfe990
mrlee <lkt900520@gmail.com>
2024-11-09 18:56:52 +0900

    feat: 액추에이터, 프로메테우스 환경 설정 추가
    - 테스트 컨트롤러와 해당 설정 코드 추가

--- src/main/java/mrlee/kt_mall/KtMallApplication.java
+++ src/main/java/mrlee/kt_mall/KtMallApplication.java
@@ -1,7 +1,9 @@
 package mrlee.kt_mall;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,6 +12,11 @@ public class KtMallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KtMallApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
